f32667c4aa5e87351da9ff0cdbe56fd7de0a199c
Haris Mumtaz <hmumtaz@nisum.com>
2018-09-22 15:43:56 +0500

    initiated flight tracking system on c# mvc 4

---
+++ FlightQuery/Airpot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightQuery
+{
+    public class Airpot
+    {
+        public int Airportname { get; set; }
+        public int City { get; set; }
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
+        public int info { get; set; }
+    
+
+    
+    }
+}
---
+++ FlightQuery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FlightQuery.MvcApplication" Language="C#" %>
